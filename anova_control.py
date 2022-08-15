7c5c196e0b391e378ee2cbf5ac5ae4d25382ebde
mpsamuels <email@martinsamuels.me.uk>
2022-08-15 22:12:52 +0100

    Add exception handling
    Add exception handling to return 'unkown' values in JSON if cooker is offline to avoid filling Home Assistant logs with JSON return empty errors

--- anova_control.py
+++ anova_control.py
@@ -32,36 +32,57 @@ def check_arg(args=None):
 			
 if __name__ == '__main__':
 	if check_arg(sys.argv[1:]) is not None :
-		#Connect to Anova, authenticate and collect cooker status
-		o, i, u, p = check_arg(sys.argv[1:])
-		cooker = AnovaCooker.AnovaCooker(i)
-		cooker.authenticate(u, p)
-		cooker.update_state
-		if (o == "r") or (o == "raw") :
-			#Return cooker status values in raw text from
-			print('job_status = ', (cooker.job_status), ', job_time_remaining = ', (cooker.job_time_remaining), ', heater_duty_cycle = ', (cooker.heater_duty_cycle), \
-			', motor_duty_cycle = ', (cooker.motor_duty_cycle), ', wifi_connected = ', (cooker.wifi_connected), ', wifi_ssid = ', (cooker.wifi_ssid), ', water_leak = ', (cooker.water_leak), \
-			', water_level_low = ', (cooker.water_level_low), ', water_level_critical = ', (cooker.water_level_critical), ', heater_temp = ', (cooker.heater_temp), ', triac_temp = ', (cooker.triac_temp), \
-			', water_temp = ', (cooker.water_temp))
-		elif (o == "j") or (o == "json") :
-			#Build JSON array
-			data = {}
-			data['job_status'] = cooker.job_status
-			data['job_time_remaining'] = cooker.job_time_remaining
-			data['heater_duty_cycle'] = cooker.heater_duty_cycle
-			data['motor_duty_cycle'] = cooker.motor_duty_cycle
-			data['wifi_connected'] = cooker.wifi_connected
-			data['wifi_ssid'] = cooker.wifi_ssid
-			data['water_leak'] = cooker.water_leak
-			data['water_level_low'] = cooker.water_level_low
-			data['water_level_critical'] = cooker.water_level_critical
-			data['heater_temp'] = cooker.heater_temp
-			data['triac_temp'] = cooker.triac_temp
-			data['water_temp'] = cooker.water_temp
-			json_data = json.dumps(data)
-			
-			#Output the JSON
-			print(json_data)
+		try:
+			#Connect to Anova, authenticate and collect cooker status
+			o, i, u, p = check_arg(sys.argv[1:])
+			cooker = AnovaCooker.AnovaCooker(i)
+			cooker.authenticate(u, p)
+			cooker.update_state
+			if (o == "r") or (o == "raw") :
+				#Return cooker status values in raw text from
+				print('job_status = ', (cooker.job_status), ', job_time_remaining = ', (cooker.job_time_remaining), ', heater_duty_cycle = ', (cooker.heater_duty_cycle), \
+				', motor_duty_cycle = ', (cooker.motor_duty_cycle), ', wifi_connected = ', (cooker.wifi_connected), ', wifi_ssid = ', (cooker.wifi_ssid), ', water_leak = ', (cooker.water_leak), \
+				', water_level_low = ', (cooker.water_level_low), ', water_level_critical = ', (cooker.water_level_critical), ', heater_temp = ', (cooker.heater_temp), ', triac_temp = ', (cooker.triac_temp), \
+				', water_temp = ', (cooker.water_temp))
+			elif (o == "j") or (o == "json") :
+				#Build JSON array
+				data = {}
+				data['job_status'] = cooker.job_status
+				data['job_time_remaining'] = cooker.job_time_remaining
+				data['heater_duty_cycle'] = cooker.heater_duty_cycle
+				data['motor_duty_cycle'] = cooker.motor_duty_cycle
+				data['wifi_connected'] = cooker.wifi_connected
+				data['wifi_ssid'] = cooker.wifi_ssid
+				data['water_leak'] = cooker.water_leak
+				data['water_level_low'] = cooker.water_level_low
+				data['water_level_critical'] = cooker.water_level_critical
+				data['heater_temp'] = cooker.heater_temp
+				data['triac_temp'] = cooker.triac_temp
+				data['water_temp'] = cooker.water_temp
+				json_data = json.dumps(data)
+				
+				#Output the JSON
+				print(json_data)
+		except:
+				#Build JSON array
+				data = {}
+				data['job_status'] = 'unknown'
+				data['job_time_remaining'] = 'unknown'
+				data['heater_duty_cycle'] = 'unknown'
+				data['motor_duty_cycle'] = 'unknown'
+				data['wifi_connected'] = 'unknown'
+				data['wifi_ssid'] = 'unknown'
+				data['water_leak'] = 'unknown'
+				data['water_level_low'] = 'unknown'
+				data['water_level_critical'] = 'unknown'
+				data['heater_temp'] = 'unknown'
+				data['triac_temp'] = 'unknown'
+				data['water_temp'] = 'unknown'
+				json_data = json.dumps(data)
+				
+				#Output the JSON
+				print(json_data)
+
 	else:
 		parser=argparse.ArgumentParser('Script to control Anova cooker. For use with pyanova-api (https://github.com/ammarzuberi/pyanova-api)')
 		parser.print_help(sys.stderr)
